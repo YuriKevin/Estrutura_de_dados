820bb77adadf9fddee0ae3545df4cb058f8b71fb
Yuri Kevin <91747141+YuriKevin@users.noreply.github.com>
2025-09-20 14:15:21 -0300

    feat: Interface com os contratos de uma pilha estática.

---
+++ pilhas/Estática/StackOperations.java
@@ -0,0 +1,11 @@
+package pilhas.Estática;
+
+public interface StackOperations {
+    void push(Object data);
+    Object pop();
+    Object peek();
+    void update(Object item);
+    boolean isFull();
+    boolean isEmpty();
+    String print();
+}
