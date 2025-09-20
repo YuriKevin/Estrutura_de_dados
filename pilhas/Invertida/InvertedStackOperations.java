2952fda64961a2afad550ad31ee7a5aa8b10ff84
Yuri Kevin <91747141+YuriKevin@users.noreply.github.com>
2025-09-20 14:17:25 -0300

    feat: interface com contratos da pilha invertida.

---
+++ pilhas/Invertida/InvertedStackOperations.java
@@ -0,0 +1,11 @@
+package pilhas.Invertida;
+
+public interface InvertedStackOperations {
+    void push(Object data);
+    Object pop();
+    Object peek();
+    void update(Object item);
+    boolean isFull();
+    boolean isEmpty();
+    String print();
+}
