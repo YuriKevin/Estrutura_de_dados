782bb6b6474997b3fc734e4eeb4406d2fbbb27fd
Yuri Kevin <91747141+YuriKevin@users.noreply.github.com>
2025-09-20 14:19:53 -0300

    feat: implementação da classe pilha invertida.

---
+++ pilhas/Invertida/InvertedStack.java
@@ -0,0 +1,71 @@
+package pilhas.Invertida;
+
+public class InvertedStack implements InvertedStackOperations{
+    private int topIndex;
+    private Object[] data;
+
+    public InvertedStack(){
+        this(12);
+    }
+
+    public InvertedStack(int length){
+        topIndex = length;
+        data = new Object[length];
+    }
+
+    @Override
+    public void push(Object item){
+        if(!isFull()){
+            topIndex--;
+            data[topIndex]=item;
+        }else {
+            System.err.println("A pilha está cheia.");
+        }
+    }
+    public Object pop(){
+        Object topData=null;
+        if(!isEmpty()){
+            topData=data[topIndex];
+            topIndex++;
+        }else {
+            System.err.println("A pilha está vazia.");
+        }
+        return topData;
+    };
+    public Object peek(){
+        if(!isEmpty()){
+            return data[topIndex];
+        }else {
+            System.out.println("A pilha está vazia!");
+            return null;
+        }
+
+    };
+    public void update(Object item){
+        if (!isEmpty()){
+            data[topIndex]=item;
+        }
+        else {
+            System.out.println("A pilha está vazia.");
+        }
+
+    }
+    public boolean isFull(){
+        return (topIndex==0);
+    };
+    public boolean isEmpty(){
+        return (topIndex== data.length);
+
+    };
+    public String print(){
+        String resultado="[";
+        for (int i = topIndex; i>=0; i--) {
+            if (i==0){
+                resultado+=data[i];
+            }else{
+                resultado += data[i]+",";
+            }
+        }
+        return resultado+"]";
+    };
+}
