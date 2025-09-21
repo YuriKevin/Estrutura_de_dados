4743f547c3642272b9e470638fa43fe17bbb9ce3
Yuri Kevin <91747141+YuriKevin@users.noreply.github.com>
2025-09-21 15:23:40 -0300

    feat: Questão 7.

---
+++ listas_de_exercicios/unidade_1/lista_1/Q7.java
@@ -0,0 +1,29 @@
+package listas_de_exercicios.unidade_1.lista_1;
+
+import java.util.Scanner;
+
+public class Q7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[5];
+        int soma = 0;
+        int multiplicacao = 1;
+
+        System.out.println("Digite 5 números inteiros:");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
+            numeros[i] = scanner.nextInt();
+
+            soma += numeros[i];
+            multiplicacao *= numeros[i];
+        }
+
+        System.out.println("\nNúmeros digitados:");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print(numeros[i] + " ");
+        }
+        System.out.println("\n\nSoma = " + soma);
+        System.out.println("Multiplicação = " + multiplicacao);
+        scanner.close();
+    }
+}
