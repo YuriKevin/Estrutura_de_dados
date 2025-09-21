fa07a5e6877eca5af4090691c62db3e01911e5ff
Yuri Kevin <91747141+YuriKevin@users.noreply.github.com>
2025-09-21 16:27:39 -0300

    feat: Questão 1.

---
+++ listas_de_exercicios/unidade_1/lista_2/Q1.java
@@ -0,0 +1,12 @@
+package listas_de_exercicios.unidade_1.lista_2;
+
+public class Q1 {
+    public static void main(String[] args) {
+        int[] numeros = {10, 20, 30, 40, 50};
+
+        System.out.println("Elementos do array:");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
+    }
+}
