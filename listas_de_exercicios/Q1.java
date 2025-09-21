eeeb84061976e63b48623d7f1db5c559e14f3fe7
Yuri Kevin <91747141+YuriKevin@users.noreply.github.com>
2025-09-21 14:59:32 -0300

    feat: Questão 1.

---
+++ listas_de_exercicios/Q1.java
@@ -0,0 +1,20 @@
+package listas_de_exercicios;
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[5];
+        
+        System.out.println("Digite 5 números inteiros:");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
+            numeros[i] = scanner.nextInt();
+        }
+        System.out.println("\nNúmeros digitados:");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Posição " + i + ": " + numeros[i]);
+        }
+        scanner.close();
+    }
+}
