faeb9a0d5d052c0131f096470811c254525262f4
Yuri Kevin <91747141+YuriKevin@users.noreply.github.com>
2025-09-21 15:03:01 -0300

    feat: Questão 2.

---
+++ listas_de_exercicios/Q2.java
@@ -0,0 +1,21 @@
+package listas_de_exercicios;
+import java.util.Scanner;
+
+public class Q2 {
+   public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double[] numeros = new double[10];
+        
+        System.out.println("Digite 10 números de ponto flutuante:");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
+            numeros[i] = scanner.nextDouble();
+        }
+        System.out.println("\nNúmeros na ordem inversa:");
+        for (int i = numeros.length - 1; i >= 0; i--) {
+            System.out.println("Posição " + i + ": " + numeros[i]);
+        }
+        scanner.close();
+    }
+}
+
