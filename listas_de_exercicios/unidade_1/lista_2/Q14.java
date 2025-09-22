92f0c7a8c33bafdb1608dbe58429ecbfa265050c
Yuri Kevin <91747141+YuriKevin@users.noreply.github.com>
2025-09-21 21:24:55 -0300

    feat: Questão 14.

---
+++ listas_de_exercicios/unidade_1/lista_2/Q14.java
@@ -0,0 +1,35 @@
+package listas_de_exercicios.unidade_1.lista_2;
+import java.util.Scanner;
+
+public class Q14 {
+   public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o tamanho do array: ");
+        int n = scanner.nextInt();
+
+        int[] numeros = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Digite o número " + (i + 1) + ": ");
+            numeros[i] = scanner.nextInt();
+        }
+
+        // Bubble Sort
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (numeros[j] > numeros[j + 1]) {
+                    int temp = numeros[j];
+                    numeros[j] = numeros[j + 1];
+                    numeros[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("\nArray em ordem crescente:");
+        for (int num : numeros) {
+            System.out.print(num + " ");
+        }
+        scanner.close();
+    }
+}
