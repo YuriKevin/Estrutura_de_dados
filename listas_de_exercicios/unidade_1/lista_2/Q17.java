81484f235f3600160ba40c192c03eeac3bab6b0d
Yuri Kevin <91747141+YuriKevin@users.noreply.github.com>
2025-09-21 21:29:40 -0300

    feat: Questão 17.

---
+++ listas_de_exercicios/unidade_1/lista_2/Q17.java
@@ -0,0 +1,42 @@
+package listas_de_exercicios.unidade_1.lista_2;
+
+import java.util.Scanner;
+
+public class Q17 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o tamanho dos arrays: ");
+        int n = scanner.nextInt();
+
+        int[] array1 = new int[n];
+        int[] array2 = new int[n];
+
+        System.out.println("Preenchendo o primeiro array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Digite o elemento " + (i + 1) + ": ");
+            array1[i] = scanner.nextInt();
+        }
+
+        System.out.println("Preenchendo o segundo array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Digite o elemento " + (i + 1) + ": ");
+            array2[i] = scanner.nextInt();
+        }
+
+        boolean iguais = true;
+        for (int i = 0; i < n; i++) {
+            if (array1[i] != array2[i]) {
+                iguais = false;
+                break;
+            }
+        }
+
+        if (iguais) {
+            System.out.println("\nOs arrays são iguais.");
+        } else {
+            System.out.println("\nOs arrays são diferentes.");
+        }
+        scanner.close();
+    }
+}
